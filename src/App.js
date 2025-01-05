290c09780cc739ef0737e22b7431f38d648617a9
Nataliia Kovalova <natusya.zira@outlook.com>
2025-01-05 20:54:29 +0200

    Style main page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './assets/styles/style.css';
-import image from './assets/images/Logo.png';
+import image from './assets/images/Logo-main.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <img src={image} width="100"></img>
+      <header>
+      <img src={image} width="50"></img>
        <a href="./">Paw App</a>
       </header>
+      <div class="container"> 
+      <section class="menu-container">
+         <div class="card gallery"><button class="btn"type="button">Gallery</button></div> 
+         <div class="card breeds"><button class="btn" type="button">Breeds</button></div>
+         <div class="card voting"><button class="btn" type="button">Voting</button></div> 
+      </section>
+      <section class="main-container">
+        
+      </section>
+      </div>
     </div>
   );
 }
