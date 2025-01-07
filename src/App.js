1eca29e3fcc32387fc450c33f1d75b58390009d9
Nataliia Kovalova <natusya.zira@outlook.com>
2025-01-07 19:23:42 +0200

    Style navigation's container

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import './assets/styles/style.css';
 import image from './assets/images/Logo-main.png';
+import sad from './assets/images/smiley-sad.svg';
+import smiley from './assets/images/smiley.svg';
+import heart from './assets/images/heart.svg';
 
 function App() {
   return (
     <div className="App">
+      <div className="container"> 
       <header>
       <img src={image} width="50"></img>
        <a href="./">Paw App</a>
       </header>
-      <div class="container"> 
-      <section class="menu-container">
-         <div class="card gallery"><button class="btn"type="button">Gallery</button></div> 
-         <div class="card breeds"><button class="btn" type="button">Breeds</button></div>
-         <div class="card voting"><button class="btn" type="button">Voting</button></div> 
+      
+      <section className="menu-container">
+         <div className="card gallery"><button className="btn"type="button">Gallery</button></div> 
+         <div className="card breeds"><button className="btn" type="button">Breeds</button></div>
+         <div className="card voting"><button className="btn" type="button">Voting</button></div> 
       </section>
-      <section class="main-container">
-        
+      <div className="navigation">
+        <input type="text" placeholder="Search for breeds"/>
+        <div className="nav-icons">
+          <img className="sad" src={sad}/>
+          <img className="smiley" src={smiley}/>
+          <img className="heart" src={heart}/>
+        </div>
+      </div>
+      <section className="main-container">   
       </section>
       </div>
     </div>
